d1b525a0f91e2571c19520fac1f4c28ba8ac5782
neulii <neulii@gmx.at>
2019-04-17 19:08:53 +0200

    and

--- tests.cpp
+++ tests.cpp
@@ -1,12 +1,43 @@
 #include <iostream>
 
+using namespace std;
+
 int game();
 
 int main() {
     
-    std::cout << "super"<< std::endl;
+   
     
-    game();
+    //game();
+    
+    int n = 20;
+    
+    int *test;
+    
+    test = new int[n];
+   
+    for(int i = 0; i<n; i++){
+        test[i] = i;
+    }
+    
+    
+    
+    for(int i = 0; i<n; i++){
+        std::cout << test[i] << std::endl;
+        
+    }
+    
+    std::cout << endl << endl;
+    
+    std::cout << *(test+1) << endl;
+    
+    
+    
+    
+    
+    
+    
+    
     
     
     
