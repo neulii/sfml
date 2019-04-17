f8009116938d7eec95472a953f791e36c6853d4c
neulii <neulii@gmx.at>
2019-04-17 15:44:16 +0200

    add new files

---
+++ Functions.cpp
@@ -0,0 +1,9 @@
+
+#include "Functions.hpp"
+
+int add(int a, int b){
+    int sum;
+    sum = a+b;
+    
+    return sum;
+}
---
+++ Functions.hpp
@@ -0,0 +1,15 @@
+//
+//  Functions.hpp
+//  sfml template
+//
+//  Created by Stefan Neumüller on 17.04.19.
+//  Copyright © 2019 neulii. All rights reserved.
+//
+
+#ifndef Functions_hpp
+#define Functions_hpp
+
+int add(int a, int b);
+
+
+#endif /* Functions_hpp */
